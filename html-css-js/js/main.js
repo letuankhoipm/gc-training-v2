8fef2a34352aa5c8f19bf8c1aa9dee1805bb9d10
Smainee <nvsmai.ne@gmail.com>
2021-10-19 15:10:49 +0700

    fix sidebar

---
+++ html-css-js/js/main.js
@@ -0,0 +1,6 @@
+function hiddenMainLeft() {
+    var hidden = document.getElementById('hidden-main-left');
+    if (hidden.style.display === 'none') hidden.style.display = 'block';
+	else hidden.style.display = 'none';
+}
+
